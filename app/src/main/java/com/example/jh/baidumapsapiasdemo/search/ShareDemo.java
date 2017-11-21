de2d21d2f3ea547b9fdf4cb3daffdb3970f71c52
JinHui <hui.jin@boatchina.net>
2017-11-21 21:07:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/baidumapsapiasdemo/search/ShareDemo.java
@@ -0,0 +1,26 @@
+package com.example.jh.baidumapsapiasdemo.search;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.jh.baidumapsapiasdemo.R;
+
+
+public class ShareDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_demo);
+    }
+
+    public void startShareDemo(View view) {
+        Intent intent = new Intent();
+        intent.setClass(this, ShareDemoActivity.class);
+        startActivity(intent);
+
+    }
+
+}
